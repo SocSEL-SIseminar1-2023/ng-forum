783d284e2f94b88f3149099bf9836d76efd50ed5
SI Seminar TA <32835505+kohmoli@users.noreply.github.com>
2023-05-30 13:01:36 +0900

    refactoring

--- judge.py
+++ judge.py
@@ -1,27 +1,45 @@
+"""NG行動名とNG判定関数を提供する"""
 import random
 import re
 
 
 
 def is_more_than_30_chars(message):
+    """メッセージが30文字以上か判定する
+
+    len()を用いてメッセージが30文字以上か判定し、bool値を返す
+
+    :param message: 判定するメッセージ
+    :type message: str
+    :return: メッセージが30文字以上ならばTrue、それ以外でFalse
+    :rtype: bool
+    """
     return len(message) >= 30
 
 
 
 # bug: 大きな素数を入れると処理に時間がかかる（例 2147483647）
 def contains_prime_number(message):
-    # 文章中の整数nを順番に取り出す
-    for n in map(int, re.findall(r'\d+', message)):
+    """メッセージが素数を含むか判定する
+
+    メッセージに1つ以上の素数が含まれるか判定し、bool値を返す
+
+    :param message: 判定するメッセージ
+    :type message: str
+    :return: メッセージに1つ以上の素数があればTrue、それ以外でFalse
+    :rtype: bool
+    """
+    # 文章中の整数numberを順番に取り出す
+    for number in map(int, re.findall(r'\d+', message)):
         # nが2未満ならスキップ
-        if n < 2:
+        if number < 2:
             continue
         # nが2≦i<nで割り切れるかチェック
         is_prime = True
-        for i in range(2, n):
-            if n % i == 0:
+        for i in range(2, number):
+            if number % i == 0:
                 is_prime = False
                 break
-        
         if is_prime:
             return True
 
@@ -30,11 +48,27 @@ def contains_prime_number(message):
 
 
 def contains_junishi_animal(message):
-    return False
+    """メッセージが十二支の動物を含むか判定する
+
+    メッセージに1つ以上の十二支の動物が含まれるか判定し、bool値を返す
+
+    :param message: 判定するメッセージ
+    :type message: str
+    :return: メッセージに1つ以上の十二支の動物があればTrue、それ以外でFalse
+    :rtype: bool
+    """
+    return message in ('鼠', '牛', '...')
 
 
 
-def get_random_NG():
+def get_random_ng():
+    """ランダムなNG行動名とNG判定関数を返す
+
+    ランダムなNG行動名と、メッセージがNG行動に該当するか判定する関数を返す
+
+    :return: ランダムなNG行動名とNG判定関数
+    :rtype: (str, function)
+    """
     return random.choice([
         ('30文字以上の文章', is_more_than_30_chars),
         ('素数を含む文章', contains_prime_number),
--- server.py
+++ server.py
@@ -1,17 +1,17 @@
+"""サーバを起動し、クライアント要求に応じてレスポンスを返す"""
 from socketserver import ThreadingMixIn
 from urllib.parse import parse_qs
 from wsgiref.simple_server import make_server, WSGIServer
-
 import copy
 import hashlib
 import json
 import subprocess
 import time
-
 import judge
 
 
 
+messages = []
 """
 [
     {
@@ -23,10 +23,10 @@ import judge
     ...
 ]
 """
-messages = []
 
 
 
+players = {}
 """
 {
     クライアントから送信されるトークン文字列: {
@@ -38,18 +38,30 @@ messages = []
     ...
 ]
 """
-players = {}
 
 
 
-# URLに応じて返答する
 class App:
+    """URLに応じてレスポンスを返す
+    """
+
     def __init__(self, start_response):
         self.start_response = start_response
-    
-    
+
+
 
     def read_messages(self, token):
+        """クライアントにメッセージ一覧を返す
+
+        新しいメッセージがあるまで待機してからクライアントにメッセージ一覧を返す
+
+        :param self: このクラス
+        :type self: App
+        :param token: クライアントを判別するトークン
+        :type token: str
+        :return: バイナリ文字列化したメッセージの一覧
+        :rtype: list
+        """
         if token in players:
             # 最大1時間待つ
             for _ in range(3600):
@@ -57,30 +69,38 @@ class App:
                 if players[token]['sequence'] < len(messages):
                     break
                 time.sleep(1)
-            
             players[token]['sequence'] = len(messages)
         else:
             # 新しいプレイヤの追加
-            ng, checker = judge.get_random_NG()
+            ng_rule, checker = judge.get_random_ng()
             players[token] = {
                 'player_id': hashlib.md5(token.encode('utf-8')).hexdigest(),
-                'ng': ng,
+                'ng': ng_rule,
                 'checker': checker,
                 'sequence': len(messages)
             }
-        
         # 受信プレイヤのNG行動を隠して返答する
         result_messages = copy.deepcopy(messages)
         for message in result_messages:
             if message['player_id'] == players[token]['player_id']:
                 message['ng'] = '?'
-        
         self.start_response('200 OK', [('Content-type', 'application/json')])
         return [json.dumps(result_messages).encode('utf-8')]
 
 
 
     def insert_message(self, message):
+        """新しいメッセージをメッセージ一覧に追加する
+
+        新しいメッセージメッセージ一覧に追加し、クライアントに完了レスポンスを返す
+
+        :param self: このクラス
+        :type self: App
+        :param message: 追加するメッセージ
+        :type message: dict
+        :return: 空のバイナリ文字列
+        :rtype: list
+        """
         messages.append({
             'player_id': players[message['token']]['player_id'],
             'body': message['body'],
@@ -93,52 +113,100 @@ class App:
 
 
     def index_html(self):
+        """クライアントにindex.htmlの文章を返す
+
+        page/index.htmlを読み込み、文章をクライアントに返す
+
+        :param self: このクラス
+        :type self: App
+        :return: バイナリ文字列化したindex.htmlの文章
+        :rtype: list
+        """
         self.start_response('200 OK', [('Content-type', 'text/html')])
-        with open('page/index.html', 'r') as f:
-            return [f.read().encode('utf-8')]
+        with open('page/index.html', encoding='utf-8') as file:
+            return [file.read().encode('utf-8')]
 
 
 
     def main_js(self):
+        """クライアントにmain.jsの文章を返す
+
+        page/main.jsを読み込み、文章をクライアントに返す
+
+        :param self: このクラス
+        :type self: App
+        :return: バイナリ文字列化したmain.jsの文章
+        :rtype: list
+        """
         self.start_response('200 OK', [('Content-type', 'text/javascript')])
-        with open('page/main.js', 'r') as f:
-            return [f.read().encode('utf-8')]
+        with open('page/main.js', encoding='utf-8') as file:
+            return [file.read().encode('utf-8')]
 
 
 
     def style_css(self):
+        """クライアントにstyle.cssの文章を返す
+
+        page/style.cssを読み込み、文章をクライアントに返す
+
+        :param self: このクラス
+        :type self: App
+        :return: バイナリ文字列化したstyle.cssの文章
+        :rtype: list
+        """
         self.start_response('200 OK', [('Content-type', 'text/css')])
-        with open('page/style.css', 'r') as f:
-            return [f.read().encode('utf-8')]
+        with open('page/style.css', encoding='utf-8') as file:
+            return [file.read().encode('utf-8')]
 
 
 
     def not_found(self):
+        """クライアントにNot Foundレスポンスを返す
+
+        クライアントにNot Foundレスポンスを返す
+
+        :param self: このクラス
+        :type self: App
+        :return: 空のバイナリ文字列
+        :rtype: list
+        """
         self.start_response('404 Not Found', [])
         return b''
 
 
 
 def app(environ, start_response):
+    """クライアントの要求に応じてレスポンスを返す
+
+    クライアントの要求に応じてファイルやJSONデータを返す
+
+    :param environ: クライアントからの要求情報
+    :type environ: dict
+    :param start_response: レスポンスのステータスコードとヘッダーを設定するためのメソッド
+    :type start_response: method
+    :return: クライアントの要求に応じたバイナリ文字列
+    :rtype: list
+    """
     response = App(start_response)
     query = parse_qs(environ['QUERY_STRING'])
     # URLに応じて返答する
     if environ['PATH_INFO'] == '/':
         return response.index_html()
-    elif environ['PATH_INFO'] == '/main.js':
+    if environ['PATH_INFO'] == '/main.js':
         return response.main_js()
-    elif environ['PATH_INFO'] == '/style.css':
+    if environ['PATH_INFO'] == '/style.css':
         return response.style_css()
-    elif environ['PATH_INFO'] == '/messages':
+    if environ['PATH_INFO'] == '/messages':
         return response.read_messages(query['token'][0])
-    elif environ['PATH_INFO'] == '/post':
-        return response.insert_message(json.loads(environ['wsgi.input'].read(int(environ.get('CONTENT_LENGTH', 0)))))
-    else:
-        return response.not_found()
+    if environ['PATH_INFO'] == '/post':
+        return response.insert_message(
+            json.loads(environ['wsgi.input'].read(int(environ.get('CONTENT_LENGTH', 0))))
+        )
+    return response.not_found()
 
 
 
 # サーバを起動する
 with make_server('', 8080, app, type('', (ThreadingMixIn, WSGIServer), {})) as httpd:
-    subprocess.run('echo Serving on https://8080-$WEB_HOST', shell=True)
+    subprocess.run('echo Serving on https://8080-$WEB_HOST', shell=True, check=False)
     httpd.serve_forever()
